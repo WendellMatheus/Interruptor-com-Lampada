4758d4c2622ea786f965986f2095aa9adb0787e1
Wendell Matheus <wendellmatheus77@gmail.com>
2020-10-02 17:18:24 -0300

    Subindo page completa

---
+++ ambiente.js
@@ -0,0 +1,8 @@
+function apagar() {
+    var lampada = document.getElementById('lampadaligada');
+    lampada.src = 'OFF.jpg';
+}
+function acender() {
+    var lampada = document.getElementById('lampadaligada');
+    lampada.src = 'ON.jpg';
+}
